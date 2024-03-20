0195257e3d3886a2842660165da9386e53921548
jakelong1995 <hoanglong2579@gmail.com>
2024-03-20 21:52:23 +0700

    User Create APIs to register an account and login from the user table.

---
+++ app.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from './routes/auth.js';
+import profileRoutes from './routes/profile.js';
+import authMiddleware from './middleware/authMiddleware.js';
+
+// Load environment variables from a .env file
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Connect to MongoDB database
+const PORT = process.env.PORT || 4001;
+const uri = `mongodb+srv://${process.env.MONGODB_DB_NAME}:${process.env.MONGODB_PWD}@cluster0.djglaud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/profile', authMiddleware, profileRoutes);
+
+// Connect to MongoDB database
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to database successfully"))
+  .catch((error) => console.error("Database connection failed", error));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
