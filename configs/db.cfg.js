9ff6c96a8438406ba967adb1e10caa82d7eb341b
jakelong1995 <hoanglong2579@gmail.com>
2024-08-22 17:29:50 +0700

    update .env

--- configs/db.cfg.js
+++ configs/db.cfg.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
 const uri = `mongodb+srv://${process.env.MONGODB_DB_NAME}:${process.env.MONGODB_PWD}@cluster0.djglaud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
